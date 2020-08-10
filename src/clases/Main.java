61379a47a98da45b4f5171e2a395825d1a72c154
gabriel montes <gabymontesame@gmail.com>
2020-08-10 11:01:54 -0500

    First commit, a simple java application which allow us to open and play a java version of the nokia's classic snake game

---
+++ src/clases/Main.java
@@ -0,0 +1,15 @@
+package clases;
+
+import visual.WindowMain;
+
+/*
+    Made by brokengaby just for fun, in a saturday afternoon.
+*/
+
+public class Main {
+    
+    public static void main(String[] args) {
+        WindowMain.main();
+    }
+    
+}
